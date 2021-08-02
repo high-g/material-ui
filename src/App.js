6155b464881bcb8fc7f8c58be02760b1bb46ce00
tani <tani@1-10.com>
2021-08-02 15:28:18 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import Button from '@material-ui/core/Button'
+import { Button, Box, Container } from '@material-ui/core'
 
 function App() {
   return (
-    <div className="App">
+    <Container fixed>
+      <h1>Title h1</h1>
+      <h2>Title h2</h2>
+      <h3>Title h3</h3>
+      <h4>Title h4</h4>
+      <h5>Title h5</h5>
+      <h6>Title h6</h6>
       <Button variant="contained" color="primary">
         test
       </Button>
-    </div>
+
+      <Box m={1} p={3} color="text.primary" clone>
+        <Button variant="contained" color="primary">
+          test2
+        </Button>
+      </Box>
+
+      <Box component="span">
+        <Button variant="contained" color="primary">
+          test3
+        </Button>
+      </Box>
+    </Container>
   )
 }
 
